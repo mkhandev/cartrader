e6e99b179601ad5e727911bd1b541a6066edaae3
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2024-08-07 18:52:34 +0600

    add Handiling Client and server errors

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,6 +1,22 @@
 <script setup>
 const route = useRoute();
-const {toTitleCase} = useUtilities();
+const { toTitleCase } = useUtilities();
+
+const { cars } = useCars();
+
+const car = computed(() => {
+  return cars.find((car) => {
+    return car.id === parseInt(route.params.id);
+  })
+})
+
+if(!car.value){  
+  throw createError({
+    statusCode:404,
+    message: `Car with ID ${route.params.id} does not exist.`
+  })
+}
+
 
 useHead({
   title: toTitleCase(route.params.name),
@@ -13,11 +29,9 @@ definePageMeta({
 
 <template>
   <div>
-    <CarDetailHero />
-
-    <CarDetailAttributes />
-
-    <CarDetailDescription />
+    <CarDetailHero :car="car" />
+    <CarDetailAttributes :features="car.features" />
+    <CarDetailDescription :description="car.description" />
     <CarDetailContact />
   </div>
 </template>
