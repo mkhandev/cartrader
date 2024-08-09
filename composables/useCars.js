7d169e7a438dd4f20c137c0a3212b1a3816bd697
Md. Abdullah Al Mahmud Khan <mahmudkhan.sumon@gmail.com>
2024-08-09 12:31:39 +0600

    add car make and price filter

--- composables/useCars.js
+++ composables/useCars.js
@@ -1,5 +1,7 @@
 import cars from "../data/cars.json";
+import makes from "../data/makes.json";
+import listings from "../data/listings.json";
 
 export const useCars = () => {
-  return { cars };
+  return { cars, makes, listings };
 };
